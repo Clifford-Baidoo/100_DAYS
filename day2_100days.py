d050e14e3d97c3bad7e9580ee8302166c2f219b2
Clifford-Baidoo <baidooclifford56@gmail.com>
2025-03-12 21:11:17 +0000

    Adding days 1 and 2

---
+++ day2_100days.py
@@ -0,0 +1,8 @@
+Name = input("What is your name? ")
+Food = input("What is your favorite food? ")
+Music = input("What is your favorite music? ")
+Location = input("Where do you live? ")
+
+print("You are",Name)
+print("You're probably hungry for", Food)
+print("and you are defintely getting your grooce on to",Music,"Living in the amazing",Location)
